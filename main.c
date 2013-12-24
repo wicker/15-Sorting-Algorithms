fa5d480b18ed40c5f45ac984fc7184357400ab6f
Jenner Hanni <jeh.wicker@gmail.com>
2013-12-23 19:25:51 -0800

    Added clear to run. Added checker, print, exit, and selection sort functions.

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #define ARGS 3
 #define DATAPOINTS 10
 
+typedef enum { false, true } bool;
+
 int instructions();
-int exit_program();
+int exit_program(int);
 int selection_sort(int * data);
+int check_sorted(int * data);
+void print_array(int * data);
 
 // take arguments for sort number and data file
 int main(int argc, char *argv[]) {
@@ -18,22 +22,27 @@ int main(int argc, char *argv[]) {
   FILE * filein = fopen(argv[2],"r");
   if (filein == NULL) {
     printf("File did not open properly! Does it exist?\n");
-    exit_program();
+    exit_program(1);
     return 0;
   }
 
   int data_array[DATAPOINTS];
+  printf("======================================\n");
+  printf("\tALL SORTS OF SORTS\n");
+  printf("======================================\n\n");
+  printf("Initial Array: \n");
   for (i = 0; i < DATAPOINTS; i++) {
     fscanf(filein, "%d,", &data_array[i]);
   }
+  print_array(data_array);
 
   switch(sortnum) {
     case 1:
       printf("#1 - Selection Sort\n");
       selection_sort(data_array);
-      for (i = 0; i < DATAPOINTS; i++) {
-        printf("data_array[%d] = %d\n",i,data_array[i]);
-      }
+      
+      printf("\nFinal Sorted Array:\n");
+      print_array(data_array);
       break;
     case 2:
       printf("#2 - Insertion Sort\n");
@@ -80,22 +89,59 @@ int main(int argc, char *argv[]) {
     default:
       printf("Improper sort number. Range 1-15.\n");
   }
-      exit_program();
-
+      exit_program(check_sorted(data_array));
 }
 
 int selection_sort(int * data) {
   int i;
-  for (i = 0; i < DATAPOINTS; i++) {
-    printf("data[%d] = %d\n",i,data[i]);
-    data[i] = DATAPOINTS - i;
+  int p = 0;  // p is placed location
+  int fa = 0; // fa is found at location
+  int min = data[0];
+
+  for (p = 0; p < DATAPOINTS; p++) {
+    // get the min
+    min = data[p];
+    for (i = p; i < DATAPOINTS; i++) {
+      if (min > data[i]) {
+        min = data[i];
+        fa = i;
+      }
+    }
+    
+    // swap the min
+    data[fa] = data[p];
+    data[p] = min;
   }
+
+  return min;
+}
+
+int check_sorted(int * data) {
+  int p;
+  for (p = 0; p < DATAPOINTS-1; p++) {
+    if (data[p] > data[p+1]) {
+      printf("Array is not sorted correctly. This algorithm failed.\n");
+      return 1;
+    }
+  }
+  printf("Array sorted correctly! This algorithm did its job.\n");
   return 0;
 }
 
-int exit_program() {
+void print_array(int * data) {
+  int i;
+  for (i = 0; i < DATAPOINTS; i++) {
+    printf("%d ",data[i]);
+  }
+  printf("\n\n");
+}
 
-  printf("Exiting program.\n");
+int exit_program(int result) {
+  if (result == 1)
+    printf("Program was not successful. My bad!\n\n");
+  if (result == 0)
+    printf("Program successfully completed.\n\n");
+    printf("======================================\n");
   return 0;
 }
 
